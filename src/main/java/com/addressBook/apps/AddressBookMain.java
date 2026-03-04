415cf1b8ff252f42beabf11de6eea54db5f04eab
Lucky Pal <pallucky936@gmail.com>
2026-03-04 18:54:40 +0530

    [Lucky Pal]: modified AddressBookMain file for UC11

--- src/main/java/com/addressBook/apps/AddressBookMain.java
+++ src/main/java/com/addressBook/apps/AddressBookMain.java
@@ -117,7 +117,11 @@ public class AddressBookMain {
 		
 		return ad.countByState();
 	}
-	
+	public static List<Contacts> sortByName(String addressBookName){
+		addressBookName = addressBookName.toLowerCase();
+		AddressBook ad = addressBook.get(addressBookName);
+		return ad.sortByName();
+	}
     public static void main(String[] args ) throws IOException{
         addAddress("Book1");
         
@@ -150,7 +154,7 @@ public class AddressBookMain {
         System.out.println("\nCount people in city addressBook 1 : "+countByCity("Book1")); 
         System.out.println("\nCount people in state in addressBook 1 : "+countByState("Book1"));
         
-        
+        System.out.println("\nsorted by name : "+sortByName("Book1"));
     }
     
 }

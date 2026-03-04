9b727ba545a751f0cd72354bf947307c0863963d
Lucky Pal <pallucky936@gmail.com>
2026-03-04 19:30:25 +0530

    [Lucky Pal]: modified AddressBook file for UC13

--- src/main/java/com/addressBook/apps/AddressBook.java
+++ src/main/java/com/addressBook/apps/AddressBook.java
@@ -1,5 +1,7 @@
 package com.addressBook.apps;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -208,5 +210,12 @@ public class AddressBook {
 	   
 	   public List<Contacts> sortByZipCode(){
 		   return contacts.stream().sorted(Comparator.comparing(Contacts::getZip)).toList();
+	   }
+	   
+	   public void writeInfile(String s) throws IOException{
+		   FileOutputStream file = new FileOutputStream(s);
+		  for(Contacts c: contacts) {
+			  file.write(c.toString().getBytes());
+		  }
 	   }
 }
